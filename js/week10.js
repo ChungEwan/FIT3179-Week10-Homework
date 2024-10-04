9a4a40a7c274b631217ed0b2c0b93a96fce05e71
Ewan <126139708+ChungEwan@users.noreply.github.com>
2024-10-04 14:40:00 +0800

    Add parallel coordinates plot

--- js/week10.js
+++ js/week10.js
@@ -1,3 +1,7 @@
 var vg_1 = "js/map.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "js/parallel_coordinates_plot.vg.json";
+vegaEmbed("#parallel_coordinates_plot", vg_2, {"actions": false}).then(function(result) {
 }).catch(console.error);
