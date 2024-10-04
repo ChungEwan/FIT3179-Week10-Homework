353eb4fb68d06f0b4388bceba1ebd29e02a1043b
Ewan <126139708+ChungEwan@users.noreply.github.com>
2024-10-04 14:37:56 +0800

    Delete week10.js

--- week10.js
+++
@@ -1,7 +0,0 @@
-var vg_1 = "js/map.vg.json";
-vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
-}).catch(console.error);
-
-var vg_2 = "js/parallel_coordinates_plot.vg.json";
-vegaEmbed("#parallel_coordinates_plot", vg_2, {"actions": false}).then(function(result) {
-}).catch(console.error);
