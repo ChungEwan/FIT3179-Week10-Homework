d8c25595eb1e79972570cfd6e878bc13fdbb7224
Ewan <126139708+ChungEwan@users.noreply.github.com>
2024-10-05 14:26:40 +0800

    Changed parallel coordinate chart to bubble plot
    Completed task 1 and task 2 for week 10 homework

--- js/week10.js
+++ js/week10.js
@@ -2,6 +2,6 @@ var vg_1 = "js/map.vg.json";
 vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
 }).catch(console.error);
 
-var vg_2 = "js/parallel_coordinates_plot.vg.json";
-vegaEmbed("#parallel_coordinates_plot", vg_2, {"actions": false}).then(function(result) {
+var vg_2 = "js/bubble_plot.vg.json";
+vegaEmbed("#bubble_plot", vg_2, {"actions": false}).then(function(result) {
 }).catch(console.error);
